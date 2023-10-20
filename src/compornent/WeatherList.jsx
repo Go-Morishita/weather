175448f865b75199070ccf43161eee80698bf96a
GO0128 <go20040128@gmail.com>
2023-10-20 15:22:27 +0900

    Udate

--- src/compornent/WeatherList.jsx
+++ src/compornent/WeatherList.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const WeatherList = ({ weatherData }) => {
     return (
         <div>
-            <h2>{weatherData[0].publishingOffice}</h2>
+            <h2>{weatherData[0].timeSeries[0].areas[0].area.name}</h2>
             <ul>
             {weatherData[0].timeSeries[0].areas[0].weathers.map((weather, index) => (
                 <li key={index}>{weather}</li>
