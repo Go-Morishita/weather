5465f3b96a4945ea9e3fc52a721b675830491db9
GO0128 <go20040128@gmail.com>
2023-10-20 15:16:43 +0900

    appdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios';
-import WeatherList from './compornent/weatherList';
+import WeatherList from './compornent/WeatherList';
 
 function App() {
 
